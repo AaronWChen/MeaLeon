21a270e1588ef678d18382c51e66e4d928401fe6
Aaron W Chen <awchen2009@gmail.com>
2019-11-14 13:54:11 -0800

    Removed uninstalled library, added background color

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 from flask import Flask, send_from_directory, render_template, request, abort
-from flask_s3 import FlaskS3
 from waitress import serve
 from models.dish_predictor import find_similar_dishes
 import json
 
-# app = Flask(__name__, static_url_path="/static")
-
-s3 = FlaskS3()
-
-def start_app():
-    app = Flask(__name__, static_url_path="/photos")
-    app.config['FLASKS3_BUCKET_NAME'] = 'mealeonphotos'
-    s3.init_app(app)
-    return app
-
-app = start_app()
+app = Flask(__name__, static_url_path="/static")
 
 @app.route("/")
 def index():
