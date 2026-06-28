97adcca098c1bf79d9775dd3eb6f61519bc078d7
Aaron W Chen <awc33@cornell.edu>
2026-06-28 10:24:04 -0700

    Move MLflow out of shared postgres to sqlite

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 DB_PASSWORD=$(cat /run/secrets/db_password)
 exec mlflow server \
-  --backend-store-uri "postgresql+psycopg://docker_user:${DB_PASSWORD}@db:5432/mealeon" \
+  # --backend-store-uri "postgresql+psycopg://docker_user:${DB_PASSWORD}@db:5432/mealeon" \
+  --backend-store-uri sqlite:///mlflow/mlflow.db
   --default-artifact-root /mlflow/artifacts \
   --host 0.0.0.0 \
   --port 5001
